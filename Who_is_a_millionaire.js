0f8c4aa22f3e1702aa1b142df3eca28e5cf06f57
trantheanh3101 <trantheanhk11@gmail.com>
2024-05-02 06:15:37 +0700

    pc

--- Who_is_a_millionaire.js
+++ Who_is_a_millionaire.js
@@ -8,23 +8,23 @@ class Question{
         this.money = money;
     }
     checkAnswer(options){
-        this.options === this.trueAnswer;
+        return options === this.trueAnswer;
     }
 }
 
 // xây 10 câu hỏi và gộp vào 1 mảng chứa all các quention
 
 
-let question1 =new Question("Câu 1: Ai là cầu thủ đầu tiên giành Quả Bóng Vàng FIFA?", ["Lionel Messi", "Cristiano Ronaldo", "Pelé", "Diego Maradona"], "Pelé", "1000$");
-let question2 = new Question("Câu 2: Ai là người đã ghi bàn thắng đầu tiên trong lịch sử World Cup?", ["Pelé", "Geoff Hurst", "Bert Patenaude", "Lucien Laurent"], "Lucien Laurent", "2000$");
-let question3 = new Question("Cu 3: Câu lạc bộ nào đã giành nhiều chức vô địch Champions League nhất?", ["Real Madrid", "Barcelona", "AC Milan", "Liverpool"], "Real Madrid", "3000$");
-let question4 = new Question("Câu 4: Thành phố nào đã đăng cai World Cup nhiều nhất?", ["Paris, Pháp", "London, Anh", "Madrid, Tây Ban Nha", "Rio de Janeiro, Brazil"], "London, Anh", "4000$");
-let question5 = new Question("Câu 5: Khi nào World Cup lần đầu tiên được tổ chức?", ["1926", "1930", "1940", "1950"], "1930", "5000$");
-let question6 = new Question("Câu 6: Cầu thủ nào đã giành giải thưởng Quả Bóng Vàng FIFA nhiều nhất?", ["Lionel Messi", "Cristiano Ronaldo", "Michel Platini", "Pelé"], "Lionel Messi", "6000$");
-let question7 = new Question("Câu 7: Giải đấu nào được coi là 'giải vô địch quốc gia' cao nhất tại châu Âu?", ["UEFA Champions League", "La Liga (Tây Ban Nha)", "Premier League (Anh)", "Serie A (Ý)"], "UEFA Champions League", "7000$");
-let question8 = new Question("Câu 8: Tên đầy đủ của World Cup là gì?", ["FIFA World Championship", "FIFA World Cup", "FIFA International Cup", "FIFA Cup of Nations"], "FIFA World Cup", "8000$");
-let question9 = new Question("Câu 9: Đội bóng nào đã giành danh hiệu World Cup nhiều nhất?", ["Brazil", "Germany", "Argentina", "Italy"], "Brazil", "9000$");
-let question10 = new Question("Câu 10: Đội bóng nào được biết đến với biệt danh 'The Red Devils'?", ["Manchester United", "AC Milan", "Bayern Munich", "Chelsea"], "Manchester United", "10000$");
+let question1 = new Question("Câu 1: Ai là cầu thủ đầu tiên giành Quả Bóng Vàng FIFA?", ["A.Lionel Messi", "B.Cristiano Ronaldo", "C.Pelé", "D.Diego Maradona"], "C.Pelé", "1000$");
+let question2 = new Question("Câu 2: Ai là người đã ghi bàn thắng đầu tiên trong lịch sử World Cup?", ["A.Pelé", "B.Geoff Hurst", "C.Bert Patenaude", "D.Lucien Laurent"], "D.Lucien Laurent", "2000$");
+let question3 = new Question("Câu 3: Câu lạc bộ nào đã giành nhiều chức vô địch Champions League nhất?", ["A.Real Madrid", "B.Barcelona", "C.AC Milan", "D.Liverpool"], "A.Real Madrid", "3000$");
+let question4 = new Question("Câu 4: Thành phố nào đã đăng cai World Cup nhiều nhất?", ["A.Paris, Pháp", "B.London, Anh", "C.Madrid, Tây Ban Nha", "D.Rio de Janeiro, Brazil"], "B.London, Anh", "4000$");
+let question5 = new Question("Câu 5: Khi nào World Cup lần đầu tiên được tổ chức?", ["A.1926", "B.1930", "C.1940", "D.1950"], "B.1930", "5000$");
+let question6 = new Question("Câu 6: Cầu thủ nào đã giành giải thưởng Quả Bóng Vàng FIFA nhiều nhất?", ["A.Lionel Messi", "B.Cristiano Ronaldo", "C.Michel Platini", "D.Pelé"], "A.Lionel Messi", "6000$");
+let question7 = new Question("Câu 7: Giải đấu nào được coi là 'giải vô địch quốc gia' cao nhất tại châu Âu?", ["A.UEFA Champions League", "B.La Liga (Tây Ban Nha)", "C.Premier League (Anh)", "D.Serie A (Ý)"], "A.UEFA Champions League", "7000$");
+let question8 = new Question("Câu 8: Tên đầy đủ của World Cup là gì?", ["A.FIFA World Championship", "B.FIFA World Cup", "C.FIFA International Cup", "D.FIFA Cup of Nations"], "B.FIFA World Cup", "8000$");
+let question9 = new Question("Câu 9: Đội bóng nào đã giành danh hiệu World Cup nhiều nhất?", ["A.Brazil", "B.Germany", "C.Argentina", "D.Italy"], "A.Brazil", "9000$");
+let question10= new Question("Câu 10: Đội bóng nào được biết đến với biệt danh 'The Red Devils'?", ["A.Manchester United", "B.AC Milan", "C.Bayern Munich", "D.Chelsea"], "A.Manchester United", "10000$");
 
 let arrayQuestion = [question1,question2,question3,question4,question5,question6,question7,question8,question9,question10];
 
@@ -37,14 +37,37 @@ function showQuention(question) {
     getQuestion.setAttribute("questionDisplay",arrayQuestion.indexOf(question));
     for (let i = 0; i < 4; i++) {
         let getAnswer = document.getElementById("answer"+(i+1));
-        getAnswer.innerHTML = question.answer[i];
+        getAnswer.innerHTML = question.options[i];
     }
 }
 showQuention(question1);
-function nextQuestion(numQuestion) {
-    numQuestion++;
-    showQuention("queston" + numQuestion)
+let num = 0;
+
+function nextQuestion(n) {
+    if (n%5 === 0 && n !==0){
+        document.getElementById("question" + n).style.background = "chocolate";
+    } else {
+        document.getElementById("question" + n).style.background = "orange";
+    }
+    showQuention(arrayQuestion[n]);
 }
+
+function checkOption(valueId) {  // valueId là giá trị id của 4 câu hỏi sau khi bấm vào button
+    let answer = document.getElementById(valueId).innerHTML;
+    if (arrayQuestion[num].checkAnswer(answer)){
+        alert("đúng rồi . câu tiếp theo !!!");
+        if (num === arrayQuestion.length-1){
+            alert("bạn đã chiến thắng trò chơi !!!");
+        }
+        document.getElementById("bonus").innerHTML = "Tiền đang nhận được : " + arrayQuestion[num].money;
+        num++;
+        nextQuestion(num)
+    } else {
+        alert("sai. gameover")
+    }
+}
+
+
 
 
 
