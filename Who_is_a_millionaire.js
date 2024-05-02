0205fb2c8c6245b7b84cdad37d5ae5d4c0a1d680
trantheanh3101 <trantheanhk11@gmail.com>
2024-05-02 15:39:57 +0700

    caseLesson1

--- Who_is_a_millionaire.js
+++ Who_is_a_millionaire.js
@@ -1,4 +1,4 @@
-// tạo đối tượng Quention chứa
+// tạo đối tượng Question chứa
 //  nội dung , mảng chứa 4 cu trả lơi , câu trả lời đúng , số tiền nhận được sau khi tra lơ đúng cau đấy
 class Question{
     constructor(content,options,trueAnswer,money) {
@@ -15,23 +15,107 @@ class Question{
 // xây 10 câu hỏi và gộp vào 1 mảng chứa all các quention
 
 
-let question1 = new Question("Câu 1: Ai là cầu thủ đầu tiên giành Quả Bóng Vàng FIFA?", ["A.Lionel Messi", "B.Cristiano Ronaldo", "C.Pelé", "D.Diego Maradona"], "C.Pelé", "1000$");
-let question2 = new Question("Câu 2: Ai là người đã ghi bàn thắng đầu tiên trong lịch sử World Cup?", ["A.Pelé", "B.Geoff Hurst", "C.Bert Patenaude", "D.Lucien Laurent"], "D.Lucien Laurent", "2000$");
-let question3 = new Question("Câu 3: Câu lạc bộ nào đã giành nhiều chức vô địch Champions League nhất?", ["A.Real Madrid", "B.Barcelona", "C.AC Milan", "D.Liverpool"], "A.Real Madrid", "3000$");
-let question4 = new Question("Câu 4: Thành phố nào đã đăng cai World Cup nhiều nhất?", ["A.Paris, Pháp", "B.London, Anh", "C.Madrid, Tây Ban Nha", "D.Rio de Janeiro, Brazil"], "B.London, Anh", "4000$");
-let question5 = new Question("Câu 5: Khi nào World Cup lần đầu tiên được tổ chức?", ["A.1926", "B.1930", "C.1940", "D.1950"], "B.1930", "5000$");
-let question6 = new Question("Câu 6: Cầu thủ nào đã giành giải thưởng Quả Bóng Vàng FIFA nhiều nhất?", ["A.Lionel Messi", "B.Cristiano Ronaldo", "C.Michel Platini", "D.Pelé"], "A.Lionel Messi", "6000$");
-let question7 = new Question("Câu 7: Giải đấu nào được coi là 'giải vô địch quốc gia' cao nhất tại châu Âu?", ["A.UEFA Champions League", "B.La Liga (Tây Ban Nha)", "C.Premier League (Anh)", "D.Serie A (Ý)"], "A.UEFA Champions League", "7000$");
-let question8 = new Question("Câu 8: Tên đầy đủ của World Cup là gì?", ["A.FIFA World Championship", "B.FIFA World Cup", "C.FIFA International Cup", "D.FIFA Cup of Nations"], "B.FIFA World Cup", "8000$");
-let question9 = new Question("Câu 9: Đội bóng nào đã giành danh hiệu World Cup nhiều nhất?", ["A.Brazil", "B.Germany", "C.Argentina", "D.Italy"], "A.Brazil", "9000$");
-let question10= new Question("Câu 10: Đội bóng nào được biết đến với biệt danh 'The Red Devils'?", ["A.Manchester United", "B.AC Milan", "C.Bayern Munich", "D.Chelsea"], "A.Manchester United", "10000$");
+let question1 = new Question(
+    "Câu 1: Ai là cầu thủ đầu tiên giành Quả Bóng Vàng FIFA?",
+    ["A. Lionel Messi", "B. Cristiano Ronaldo", "C. Pelé", "D. Diego Maradona"],
+    "C. Pelé",
+    "1,000$");
+let question2 = new Question(
+    "Câu 2: Ai là người đã ghi bàn thắng đầu tiên trong lịch sử World Cup?",
+    ["A. Pelé", "B. Geoff Hurst", "C. Bert Patenaude", "D. Lucien Laurent"],
+    "D. Lucien Laurent",
+    "2,000$");
+let question3 = new Question(
+    "Câu 3: Ai là tác giả của loạt sách Harry Potter?",
+    ["A. Stephenie Meyer", "B. J.R.R. Tolkien", "C. George R.R. Martin", "D. J.K. Rowling"],
+    "D. J.K. Rowling",
+    "4,000$");
+let question4 = new Question(
+    "Câu 4: Quốc gia nào đã giành chiến thắng trong World Cup nhiều nhất?",
+    ["A. Brazil", "B. Italy", "C. Germany", "D. Argentina"],
+    "A. Brazil",
+    "6,000$");
+let question5 = new Question(
+    "Câu 5: Người đã phát minh ra World Wide Web (WWW) là ai?",
+    ["A. Tim Berners-Lee", "B. Steve Jobs", "C. Bill Gates", "D. Mark Zuckerberg"],
+    "A. Tim Berners-Lee",
+    "10,000$");
+let question6 = new Question(
+    "Câu 6: Thành phố nào được gọi là 'Thủ đô văn hóa của thế giới'?",
+    ["A. London", "B. Rome", "C. Paris", "D. New York City"],
+    "C. Paris",
+    "14,000$");
+let question7 = new Question(
+    "Câu 7: Quốc gia nào là quốc gia đầu tiên phát triển và sử dụng bom nguyên tử?",
+    ["A. Japan", "B. USSR (Nga)", "C. Germany", "D. USA"],
+    "D. USA",
+    "20,000$");
+let question8 = new Question(
+    "Câu 8: Tổ chức nào là tổ chức ngày Quốc tế Phụ nữ?",
+    ["A. UNESCO", "B. WHO", "C. UNICEF", "D. UN"],
+    "D. UN",
+    "25,000$");
+let question9 = new Question(
+    "Câu 9: Ngày nào được coi là Ngày Độc lập của Hoa Kỳ?",
+    ["A. 4th July", "B. 14th August", "C. 15th August", "D. 26th January"],
+    "A. 4th July",
+    "32,000$");
+let question10 = new Question(
+    "Câu 10: Đại dương nào lớn nhất trên trái đất?",
+    ["A. Đại Tây Dương", "B. Thái Bình Dương", "C. Ấn Độ Dương", "D. Nam Cực"],
+    "B. Thái Bình Dương",
+    "40,000$");
+let question11 = new Question(
+    "Câu 11: Quốc gia nào là quốc gia nhỏ nhất trên thế giới?",
+    ["A. Monaco", "B. San Marino", "C. Nauru", "D. Vatican City"],
+    "D. Vatican City",
+    "50,000$");
+let question12 = new Question(
+    "Câu 12: Trong hệ mặt trời, hành tinh nào là hành tinh thứ ba từ Mặt Trời?",
+    ["A. Venus", "B. Earth", "C. Mars", "D. Jupiter"],
+    "B. Earth",
+    "65,000$");
+let question13 = new Question(
+    "Câu 13: Địa danh nào được gọi là 'Cổng vào địa ngục'?",
+    ["A. Hỏa Lò", "B. Hạ Long Bay", "C. Grand Canyon", "D. Death Valley"],
+    "C. Grand Canyon",
+    "80,000$");
+let question14 = new Question(
+    "Câu 14: Ai là người đã phát minh ra máy tính cá nhân đầu tiên?",
+    ["A. Alan Turing", "B. Steve Jobs", "C. Tim Berners-Lee", "D. Charles Babbage"],
+    "D. Charles Babbage",
+    "90,000$");
+let question15 = new Question(
+    "Câu 15: Quốc gia nào đã chiến thắng trong cuộc chiến tranh thế giới thứ hai?",
+    ["A. USA", "B. Germany", "C. Japan", "D. Italy"],
+    "B. Germany",
+    "100,000$");
 
-let arrayQuestion = [question1,question2,question3,question4,question5,question6,question7,question8,question9,question10];
+let arrayQuestion = [question1,question2,question3,question4,question5,question6,question7,question8,question9,question10,question11,question12,question13,question14,question15];
 
 
 // show câu hỏi câu trar loi , trước tiên display câu hỏi 1 
 
+let time;
+let timenow = setInterval(function(){
+    document.getElementById("timeout").innerHTML = "TimeEnd : " + time + " s";
+    time--;
+    eventTimeout();
+},1000);
+
+function eventTimeout() {
+    if (time < 0){
+        clearInterval(timenow);
+        alert("hết thời gian, gameover");
+        start();
+    }
+    if (time < 5){
+        document.getElementById("timeout").style.color = "red";
+    }
+}
+
 function showQuention(question) {
+    time = 30;
     let getQuestion = document.getElementById('question');
     getQuestion.innerHTML = question.content;
     getQuestion.setAttribute("questionDisplay",arrayQuestion.indexOf(question));
@@ -58,15 +142,21 @@ function checkOption(valueId) {  // valueId là giá trị id của 4 câu hỏi
         alert("đúng rồi . câu tiếp theo !!!");
         if (num === arrayQuestion.length-1){
             alert("bạn đã chiến thắng trò chơi !!!");
+            start();
         }
         document.getElementById("bonus").innerHTML = "Tiền đang nhận được : " + arrayQuestion[num].money;
         num++;
-        nextQuestion(num)
+        nextQuestion(num);
     } else {
-        alert("sai. gameover")
+        alert("Gameover!!! Start again !!!");
+        time = 1;
+        start();
     }
 }
 
+function start() {
+    location.replace("Who_is_a_millionaire.html");
+}
 
 
 
