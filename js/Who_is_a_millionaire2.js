6c5dc72c8da64157884639261031276ce2da6522
trantheanh3101 <trantheanhk11@gmail.com>
2024-05-03 12:29:04 +0700

    end

--- js/Who_is_a_millionaire2.js
+++ js/Who_is_a_millionaire2.js
@@ -97,77 +97,77 @@ let question16 = new Question(
     "Câu 1: Ai là cầu thủ ghi bàn nhiều nhất trong lịch sử World Cup?",
     ["A. Ronaldo", "B. Miroslav Klose", "C. Pelé", "D. Lionel Messi"],
     "B. Miroslav Klose",
-    "10,000$");
+    "1,000$");
 let question17 = new Question(
-        "Câu 2: Ai là cầu thủ giành giải Quả bóng vàng FIFA nhiều nhất?",
-        ["A. Lionel Messi", "B. Cristiano Ronaldo", "C. Pelé", "D. Diego Maradona"],
-        "A. Lionel Messi",
-        "20,000$");
+    "Câu 2: Ai là cầu thủ giành giải Quả bóng vàng FIFA nhiều nhất?",
+    ["A. Lionel Messi", "B. Cristiano Ronaldo", "C. Pelé", "D. Diego Maradona"],
+    "A. Lionel Messi",
+    "2,000$");
 let question18 = new Question(
-        "Câu 3: Đội bóng nào đã vô địch Champions League nhiều nhất?",
-        ["A. Real Madrid", "B. Barcelona", "C. Bayern Munich", "D. AC Milan"],
-        "A. Real Madrid",
-        "30,000$");
+    "Câu 3: Đội bóng nào đã vô địch Champions League nhiều nhất?",
+    ["A. Real Madrid", "B. Barcelona", "C. Bayern Munich", "D. AC Milan"],
+    "A. Real Madrid",
+    "4,000$");
 let question19 = new Question(
-        "Câu 4: Ai là HLV chiến thắng nhiều nhất trong lịch sử Champions League?",
-        ["A. José Mourinho", "B. Sir Alex Ferguson", "C. Pep Guardiola", "D. Carlo Ancelotti"],
-        "A. José Mourinho",
-        "40,000$");
+    "Câu 4: Ai là HLV chiến thắng nhiều nhất trong lịch sử Champions League?",
+    ["A. José Mourinho", "B. Sir Alex Ferguson", "C. Pep Guardiola", "D. Carlo Ancelotti"],
+    "A. José Mourinho",
+    "6,000$");
 let question20 = new Question(
-        "Câu 5: Trận chung kết World Cup 2014 diễn ra ở quốc gia nào?",
-        ["A. Brazil", "B. Germany", "C. Argentina", "D. Spain"],
-        "A. Brazil",
-        "50,000$");
+    "Câu 5: Trận chung kết World Cup 2014 diễn ra ở quốc gia nào?",
+    ["A. Brazil", "B. Germany", "C. Argentina", "D. Spain"],
+    "A. Brazil",
+    "10,000$");
 let question21 = new Question(
-        "Câu 6: Ai là người đầu tiên giành giải Quả bóng vàng FIFA?",
-        ["A. Pelé", "B. Diego Maradona", "C. Johan Cruyff", "D. Lev Yashin"],
-        "A. Pelé",
-        "60,000$");
+    "Câu 6: Ai là người đầu tiên giành giải Quả bóng vàng FIFA?",
+    ["A. Pelé", "B. Diego Maradona", "C. Johan Cruyff", "D. Lev Yashin"],
+    "A. Pelé",
+    "14,000$");
 let question22 = new Question(
-        "Câu 7: Đội tuyển nào đã vô địch EURO 2020?",
-        ["A. Italy", "B. England", "C. France", "D. Portugal"],
-        "A. Italy",
-        "70,000$");
+    "Câu 7: Đội tuyển nào đã vô địch EURO 2020?",
+    ["A. Italy", "B. England", "C. France", "D. Portugal"],
+    "A. Italy",
+    "20,000$");
 let question23 = new Question(
-        "Câu 8: Cầu thủ nào đã ghi bàn nhanh nhất trong lịch sử World Cup?",
-        ["A. Hakan Şükür", "B. Ronaldo", "C. Miroslav Klose", "D. Clint Dempsey"],
-        "A. Hakan Şükür",
-        "80,000$");
+    "Câu 8: Cầu thủ nào đã ghi bàn nhanh nhất trong lịch sử World Cup?",
+    ["A. Hakan Şükür", "B. Ronaldo", "C. Miroslav Klose", "D. Clint Dempsey"],
+    "A. Hakan Şükür",
+    "25,000$");
 let question24 = new Question(
-        "Câu 9: Ai là người giành Quả bóng vàng FIFA 2022?",
-        ["A. Cristiano Ronaldo", "B. Lionel Messi", "C. Robert Lewandowski", "D. Kylian Mbappé"],
-        "C. Robert Lewandowski",
-        "90,000$");
+    "Câu 9: Ai là người giành Quả bóng vàng FIFA 2022?",
+    ["A. Cristiano Ronaldo", "B. Lionel Messi", "C. Robert Lewandowski", "D. Kylian Mbappé"],
+    "C. Robert Lewandowski",
+    "32,000$");
 let question25 = new Question(
-        "Câu 10: Trong lịch sử World Cup, đội bóng nào đã vô địch nhiều nhất?",
-        ["A. Brazil", "B. Germany", "C. Italy", "D. Argentina"],
-        "A. Brazil",
-        "100,000$");
+    "Câu 10: Trong lịch sử World Cup, đội bóng nào đã vô địch nhiều nhất?",
+    ["A. Brazil", "B. Germany", "C. Italy", "D. Argentina"],
+    "A. Brazil",
+    "40,000$");
 let question26 = new Question(
-        "Câu 11: Ai là cầu thủ ghi bàn nhiều nhất trong một kỳ EURO?",
-        ["A. Cristiano Ronaldo", "B. Michel Platini", "C. Alan Shearer", "D. Antoine Griezmann"],
-        "A. Cristiano Ronaldo",
-        "110,000$");
+    "Câu 11: Ai là cầu thủ ghi bàn nhiều nhất trong một kỳ EURO?",
+    ["A. Cristiano Ronaldo", "B. Michel Platini", "C. Alan Shearer", "D. Antoine Griezmann"],
+    "A. Cristiano Ronaldo",
+    "50,000$");
 let question27 = new Question(
-        "Câu 12: Đội bóng nào đã vô địch World Cup đầu tiên?",
-        ["A. Brazil", "B. Uruguay", "C. Italy", "D. Argentina"],
-        "B. Uruguay",
-        "120,000$");
+    "Câu 12: Đội bóng nào đã vô địch World Cup đầu tiên?",
+    ["A. Brazil", "B. Uruguay", "C. Italy", "D. Argentina"],
+    "B. Uruguay",
+    "65,000$");
 let question28 = new Question(
-        "Câu 13: Ai là người đạt cúp vàng World Cup nhiều nhất?",
-        ["A. Pelé", "B. Diego Maradona", "C. Zinedine Zidane", "D. Cafu"],
-        "A. Pelé",
-        "130,000$");
+    "Câu 13: Ai là người đạt cúp vàng World Cup nhiều nhất?",
+    ["A. Pelé", "B. Diego Maradona", "C. Zinedine Zidane", "D. Cafu"],
+    "A. Pelé",
+    "80,000$");
 let question29 = new Question(
-        "Câu 14: Ai là cầu thủ ghi bàn đầu tiên trong lịch sử Premier League?",
-        ["A. Alan Shearer", "B. Teddy Sheringham", "C. Eric Cantona", "D. Andrew Cole"],
-        "C. Eric Cantona",
-        "140,000$");
+    "Câu 14: Ai là cầu thủ ghi bàn đầu tiên trong lịch sử Premier League?",
+    ["A. Alan Shearer", "B. Teddy Sheringham", "C. Eric Cantona", "D. Andrew Cole"],
+    "C. Eric Cantona",
+    "90,000$");
 let question30 = new Question(
-        "Câu 15: Ai là cầu thủ đã giành nhiều giải thưởng Quả bóng vàng FIFA nhất?",
-        ["A. Lionel Messi", "B. Cristiano Ronaldo", "C. Michel Platini", "D. Zinedine Zidane"],
-        "A. Lionel Messi",
-        "150,000$");
+    "Câu 15: Ai là cầu thủ đã giành nhiều giải thưởng Quả bóng vàng FIFA nhất?",
+    ["A. Lionel Messi", "B. Cristiano Ronaldo", "C. Michel Platini", "D. Zinedine Zidane"],
+    "A. Lionel Messi",
+    "100,000$");
 
 let arrayQuestion2 =[question16,question17,question18,question19,question20,question21,question22,question23,question24,question25,question26,question27,question28,question29,question30];
 
@@ -188,9 +188,9 @@ function eventTimeout() {
         document.getElementById("timeout").style.color = "red";
     }
 }
-    let musicBackground = new Audio("media/audio5.wav");
-    let musicBackground1 = new Audio("media/audio1.wav");
-    let musicLose = new Audio("media/Lose.mp3")
+let musicBackground = new Audio("media/audio5.wav");
+let musicBackground1 = new Audio("media/audio1.wav");
+let musicLose = new Audio("media/Lose.mp3")
 
 // show câu hỏi câu trar loi , trước tiên display câu hỏi 1
 function showQuention(question) {
@@ -198,13 +198,13 @@ function showQuention(question) {
     musicBackground.play();
     let getQuestion = document.getElementById('question');
     getQuestion.innerHTML = question.content;
-    getQuestion.setAttribute("questionDisplay", arrayQuestion.indexOf(question));
+    getQuestion.setAttribute("questionDisplay", arrayQuestion2.indexOf(question));
     for (let i = 0; i < 4; i++) {
         let getAnswer = document.getElementById("answer"+(i+1));
         getAnswer.innerHTML = question.options[i];
     }
 }
-showQuention(question1);
+showQuention(question16);
 
 let num = 0;
 
@@ -214,21 +214,21 @@ function nextQuestion(n) {
     } else {
         document.getElementById("question" + n).style.background = "orange";
     }
-    showQuention(arrayQuestion[n]);
+    showQuention(arrayQuestion2[n]);
 }
 
 function checkOption(valueId) {  // valueId là giá trị id của 4 câu hỏi sau khi bấm vào button
     let answer = document.getElementById(valueId).innerHTML;
-    if (arrayQuestion[num].checkAnswer(answer)){
+    if (arrayQuestion2[num].checkAnswer(answer)){
         musicBackground.pause();
         musicBackground.currentTime = 0;
         musicBackground1.play();
         alert("đúng rồi . câu tiếp theo !!!");
-        if (num === arrayQuestion.length-1){
+        if (num === arrayQuestion2.length-1){
             alert("bạn đã chiến thắng trò chơi !!!");
             start();
         }
-        document.getElementById("bonus").innerHTML = "Tiền đang nhận được : " + arrayQuestion[num].money;
+        document.getElementById("bonus").innerHTML = "Tiền đang nhận được : " + arrayQuestion2[num].money;
         num++;
         nextQuestion(num);
     } else {
